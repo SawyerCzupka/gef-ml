d54583543b34b5796e4a5a491f76979b1822d91c
Sawyer <sawyer@teamculture.ai>
2024-02-28 15:15:07 -0500

    Refactor ingestion pipeline and add streaming functionality

--- gef_ml/ingestion.py
+++ gef_ml/ingestion.py
@@ -21,18 +21,47 @@ import os
 def get_pipeline(
     vector_store: BasePydanticVectorStore | None,
     together_embed_model_name: str = "togethercomputer/m2-bert-80M-32k-retrieval",
-):
+) -> IngestionPipeline:
     if os.environ.get("TOGETHER_API_KEY") is None:
         raise ValueError("TOGETHER_API_KEY environment variable must be set")
 
+    transformations = [
+        SimpleFileNodeParser(),
+        SentenceSplitter(
+            chunk_size=384, chunk_overlap=64
+        ),  # Ensures that the chunks are not too large
+        TogetherEmbedding(
+            model_name=together_embed_model_name,
+        ),
+    ]
+
     return IngestionPipeline(
-        transformations=[
-            SimpleFileNodeParser(),
-            SentenceSplitter(chunk_size=384, chunk_overlap=64),
-            TogetherEmbedding(
-                model_name=together_embed_model_name,
-                # api_key=os.environ.get["TOGETHER_API_KEY"],  # make sure this is set in the environment
-            ),
-        ],
+        transformations=transformations,
         vector_store=vector_store,
     )
+
+
+class StreamingIngestion:
+    """
+    This class is used to stream data into the vector DB in chunks as opposed to loading every document in the directory and calling the pipeline on the whole thing. Within each chunk, the documents are loaded in parallel.
+
+    This class assumes that the data is in a directory with subdirectories for each project. Each subdirectory contains the files for each project.
+    """
+
+    def __init__(
+        self, directory: str, vector_store: BasePydanticVectorStore | None = None
+    ):
+        self.directory = directory
+        self.vector_store = vector_store
+        self.pipeline = get_pipeline(vector_store)
+
+    def _ingest_project_id(self, project_id: str):
+        """
+        Ingests the data for a single project.
+        """
+        project_dir = os.path.join(
+            self.directory, project_id
+        )  # Get the project ID subdirectory
+        reader = SimpleDirectoryReader(project_dir)  # Reads the project ID subdirectory
+        
+        documents = reader.
