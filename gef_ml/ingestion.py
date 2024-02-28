e68a57d48827d20bc74dd1263ff5c43a4878fd76
Sawyer <sawyerczupka@gmail.com>
2024-02-28 16:07:52 -0500

    Refactor ingestion module and add progress logging

--- gef_ml/ingestion.py
+++ gef_ml/ingestion.py
@@ -7,15 +7,17 @@ Functions:
 - load_data: Loads the chunks into a vector DB.
 """
 
-from llama_index.core.ingestion import IngestionPipeline
-from llama_index.embeddings.together import TogetherEmbedding
-from llama_index.core import SimpleDirectoryReader
-from llama_index.core.node_parser import SimpleFileNodeParser
-from llama_index.core.node_parser import SentenceSplitter
-
-from llama_index.core.data_structs import BasePydanticVectorStore
-
+import logging
 import os
+
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core.data_structs import BasePydanticVectorStore
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.node_parser import SentenceSplitter, SimpleFileNodeParser
+from llama_index.embeddings.together import TogetherEmbedding
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
 
 
 def get_pipeline(
@@ -48,6 +50,8 @@ class StreamingIngestion:
     This class assumes that the data is in a directory with subdirectories for each project. Each subdirectory contains the files for each project.
     """
 
+    WORKERS_PER_CHUNK = 4
+
     def __init__(
         self, directory: str, vector_store: BasePydanticVectorStore | None = None
     ):
@@ -55,13 +59,44 @@ class StreamingIngestion:
         self.vector_store = vector_store
         self.pipeline = get_pipeline(vector_store)
 
-    def _ingest_project_id(self, project_id: str):
+    def _ingest_project_id(self, project_id: str, show_progress: bool = True):
         """
         Ingests the data for a single project.
         """
+
         project_dir = os.path.join(
             self.directory, project_id
         )  # Get the project ID subdirectory
         reader = SimpleDirectoryReader(project_dir)  # Reads the project ID subdirectory
-        
-        documents = reader.
+
+        documents = reader.load_data()  # Load the documents
+
+        logger.debug(f"Loaded {len(documents)} documents for project {project_id}.")
+
+        self.pipeline.run(
+            show_progress=show_progress,
+            documents=documents,
+            num_workers=self.WORKERS_PER_CHUNK,
+        )  # import directly to vector store
+
+    def ingest_directory(self, directory: str):
+        """
+        Ingests the data for the entire directory.
+        """
+
+        project_ids = [
+            f
+            for f in os.listdir(directory)
+            if os.path.isdir(os.path.join(directory, f))
+        ]
+
+        logger.info(f"Found {len(project_ids)} projects in directory {directory}.")
+
+        for project_id in tqdm(project_ids):
+            logger.info(f"Ingesting project {project_id}: Started...")
+            try:
+                self._ingest_project_id(project_id, show_progress=False)
+                logger.info(f"Ingesting project {project_id}: Done.")
+            except Exception as e:
+                logger.error(f"Ingesting project {project_id}: Failed.")
+                logger.error(e)
