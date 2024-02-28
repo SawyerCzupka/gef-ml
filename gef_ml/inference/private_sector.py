8e13ca5cf74d0b5cfe3888930a207f466966913e
Sawyer <sawyerczupka@gmail.com>
2024-02-28 16:46:45 -0500

    Added inference package and private_sector module

---
+++ gef_ml/inference/private_sector.py
@@ -0,0 +1,11 @@
+"""
+This file contains the logic determining whether a given project has involvement with the private sector.
+
+Levels of private sector involvement:
+- 1: No private sector involvement - Projects which do not involve the private sector
+- 2: Knowledge & Information Sharing - Projects in which the private sector is informed of initiatives and results
+- 3: Policy Development - Projects where the private sector is consulted as part of an intervention run by someone else
+- 4: Capacity Development - Projects focused on building the capacity of private sector actors, especially SMEs
+- 5: Finance - Projects where government or civil society engages with private sector for finance and expertise
+- 6: Industry Leadership - Projects that focus directly on the Private Sector as leader (private sector coming up with solutions; not only trained/capacitated)
+"""
