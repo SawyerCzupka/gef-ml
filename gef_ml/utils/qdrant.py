1d598c792b04a683dac99a659c499e84e5e1fd9e
Sawyer <sawyer@teamculture.ai>
2024-03-04 12:09:10 -0500

    Add qdrant_client.recreate_collection() to create a new collection with specified vector parameters

--- gef_ml/utils/qdrant.py
+++ gef_ml/utils/qdrant.py
@@ -1,4 +1,5 @@
 from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 
 
@@ -7,6 +8,11 @@ def get_qdrant_vectorstore(
 ) -> QdrantVectorStore:
     if qdrant_client is None:
         qdrant_client = QdrantClient("http://localhost:6333")
+
+    qdrant_client.recreate_collection(
+        collection_name=collection_name,
+        vectors_config=VectorParams(size=768, distance=Distance.COSINE),
+    )
 
     vector_store = QdrantVectorStore(
         client=qdrant_client, collection_name=collection_name
