87b08a8e2961a8f85a324e422dd8c962236d7bb6
Sawyer <sawyer@teamculture.ai>
2024-03-04 22:01:27 -0500

    Refactor ingestion script and add logging setup

--- scripts/ingestion_script.py
+++ scripts/ingestion_script.py
@@ -1,13 +1,12 @@
 from gef_ml.ingestion import StreamingIngestion
-from gef_ml.utils import get_qdrant_vectorstore
-
-from dotenv import load_dotenv
+from gef_ml.utils import get_qdrant_vectorstore, setup_logging
 
 import logging
 import asyncio
 
-logging.basicConfig(level=logging.INFO)
-load_dotenv()
+setup_logging()
+
+logger = logging.getLogger(__name__)
 
 
 async def main():
@@ -16,8 +15,6 @@ async def main():
     ingest_manager = StreamingIngestion(
         directory="../data/gef-6/", vector_store=vector_store
     )
-
-    # Ingest the data
 
     await ingest_manager.ingest()
 
