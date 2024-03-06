b957eb9d52dc4546f469d5519f039f7f0f6684f6
Sawyer <sawyer@teamculture.ai>
2024-03-05 21:44:55 -0500

    Refactor get_qdrant_vectorstore function to allow for custom QdrantClient

--- gef_ml/utils/qdrant.py
+++ gef_ml/utils/qdrant.py
@@ -6,13 +6,10 @@ from qdrant_client.http.models import Distance, VectorParams
 def get_qdrant_vectorstore(
     collection_name: str, qdrant_client: QdrantClient | None = None
 ) -> QdrantVectorStore:
+    """Get a QdrantVectorStore instance for a given collection name. If a QdrantClient is not provided, a new one will be created."""
+
     if qdrant_client is None:
         qdrant_client = QdrantClient("http://localhost:6333")
-
-    # qdrant_client.recreate_collection(
-    #     collection_name=collection_name,
-    #     vectors_config=VectorParams(size=768, distance=Distance.COSINE),
-    # )
 
     vector_store = QdrantVectorStore(
         client=qdrant_client, collection_name=collection_name
