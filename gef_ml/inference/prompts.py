62ec25dbba9a041858fddf586c7d3e16706ab060
Sawyer Czupka <sawyerczupka@gmail.com>
2024-03-14 11:50:52 -0400

    Refactor private sector involvement query and add prompts module

---
+++ gef_ml/inference/prompts.py
@@ -0,0 +1,10 @@
+QUERY_PRIVATE_SECTOR_INVOLVEMENT = """Determine instances of private sector involvement. Private sector involvement includes all of the following levels:
+
+- 1: No private sector involvement - Projects which do not involve the private sector
+- 2: Knowledge & Information Sharing - Projects in which the private sector is informed of initiatives and results
+- 3: Policy Development - Projects where the private sector is consulted as part of an intervention run by someone else
+- 4: Capacity Development - Projects focused on building the capacity of private sector actors, especially SMEs
+- 5: Finance - Projects where government or civil society engages with private sector for finance and expertise
+- 6: Industry Leadership - Projects that focus directly on the Private Sector as leader (private sector coming up with solutions; not only trained/capacitated)
+
+If there is private sector involvement, please provide the level of involvement and the reason for the chosen involvement level."""
