20f4f32ee7d505475e821eef6831bccc93d517b1
Sawyer Czupka <sawyerczupka@gmail.com>
2024-03-14 15:29:28 -0400

    Update descriptions in gef_ml/inference/descriptions.py

--- gef_ml/inference/descriptions.py
+++ gef_ml/inference/descriptions.py
@@ -1,23 +1,23 @@
-DESC_NO_PRIVATE_SECTOR = """"No private sector"
+DESC_NO_PRIVATE_SECTOR = """"No private sector involvement"
 
 Projects that do not involve the private sector, including projects that only mention the private sector in stakeholder identification without directly engaging with them in any way."""
 
-DESC_KNOWLEDGE_INFO_SHARING = """Category 2 ("Knowledge and Information Sharing")
+DESC_KNOWLEDGE_INFO_SHARING = """"Knowledge & Information Sharing"
 
 Projects where the private sector solely participates in awareness-raising or knowledge/information-sharing activities. If a project offers information and additional assistance to enhance the private sector's capacity for profit-making activities, it should be classified under Category 4."""
 
-DESC_POLICY_DEV = """Category 3 ("Policy development")
+DESC_POLICY_DEV = """"Policy Development"
 
 Projects where the private sector is consulted as part of an intervention run by someone else to develop policy products, including regulations, frameworks, policy reports, processes & procedures, tracking systems, plans, policies, institutional arrangements, and similar outputs. Projects may involve the private sector in its governance, e.g. by including private sector representatives in a steering or advisory committee. Private sector representatives may be present in meetings or workshops organized by a project, but they are not consulted or their views and perspectives are not considered in informing policy discussions. In such a case, the project must be classified as Category 2. For instance, projects that invite private sector representatives to attend meetings and workshops solely for awareness-raising or information purposes fall under Category 2."""
 
-DESC_CAPACITY_DEV = """Category 4 ("Capacity development")
+DESC_CAPACITY_DEV = """"Capacity Development"
 
 Projects aimed at building the capacity of private sector actors to perform their core profit-generating activities. Projects that train the private sector in auxiliary topics (such as the use of climate information to support corporate sustainability efforts and the impacts of chemical use on the health of workers) should be classified as Category 2. Projects that inform the private sector of investment opportunities related to the environment without building the capacity of the private sector to make such investments should also be classified as Category 2. Projects that deliver capacity-building initiatives but do not involve the participation of the private sector in these endeavors should be excluded from Category 4."""
 
-DESC_FINANCE = """Category 5 ("Finance")
+DESC_FINANCE = """"Finance"
 
 Projects where government or civil society engages with the private sector for finance and/or expertise, including through Public-Private Partnerships and similar financial arrangements. However, projects where the private sector solely covers routine expenses and general operational costs should not fall under Category 5. In Category 5 projects, the private sector contributes financing and/or expertise to interventions initiated by another party. If the private sector plays a leading role in a project's design and implementation, the project should be classified as Category 6."""
 
-DESC_INDUSTRY_LEADERSHIP = """Category 6 ("Industry Leadership")
+DESC_INDUSTRY_LEADERSHIP = """"Industry Leadership"
 
 Projects that directly involve the private sector as a leader in proposing solutions and advancing the project. Typically, but not always, this entails convening private sector stakeholders to generate ideas and reach agreements related to environmental commitments. In Category 6 projects, the private sector not only contributes financing and/or expertise but also spearheads the development and implementation of ideas that constitute the fundamental activities of each project."""
