79478efc9e37440c3f59189921622a0344ef22bd
Sawyer <sawyer@teamculture.ai>
2024-06-12 17:12:59 -0400

    Update private sector involvement descriptions and prompts

--- gef_ml/inference/__init__.py
+++ gef_ml/inference/__init__.py
@@ -1,3 +1,6 @@
-from .private_sector import determine_private_sector_involvement
+from .private_sector import (
+    determine_private_sector_involvement,
+    generate_private_sector_summary,
+)
 
 __all__ = ["determine_private_sector_involvement"]
--- gef_ml/inference/private_sector.py
+++ gef_ml/inference/private_sector.py
@@ -16,7 +16,7 @@ import logging
 import os
 from typing import List, Literal, Optional
 
-from llama_index.core import PromptHelper
+from llama_index.core import PromptHelper, VectorStoreIndex
 from llama_index.core.base.response.schema import PydanticResponse
 from llama_index.core.response_synthesizers import TreeSummarize
 from llama_index.core.schema import NodeWithScore
@@ -29,9 +29,17 @@ from qdrant_client.http import models as qdrant_models
 
 from gef_ml.utils import get_qdrant_vectorstore
 
-from .prompts import QUERY_PRIVATE_SECTOR_INVOLVEMENT, ResponseObject
+from .prompts import (
+    QUERY_PRIVATE_SECTOR_INVOLVEMENT,
+    QUERY_PRIVATE_SECTOR_SUMMARY,
+    QUERY_PRIVATE_SECTOR_SUMMARY_V2,
+    PrivSectorClassResponseObj,
+    PrivSectorSummaryResponseObj,
+)
 
 logger = logging.getLogger(__name__)
+
+MAX_RETRIES = 3
 
 
 if os.environ.get("TOGETHER_API_KEY") is None:
@@ -39,7 +47,7 @@ if os.environ.get("TOGETHER_API_KEY") is None:
 
 
 def retrieve_points(
-    project_id: str, collection_name: str, top_k: int = 20, mmr_threshold: float = 0.8
+    project_id: str, collection_name: str, top_k: int = 20, mmr_threshold: float = 0.7
 ) -> List[NodeWithScore]:
     """Retrieve nodes from the vector store based on a project ID."""
     embed_model = TogetherEmbedding(
@@ -81,9 +89,21 @@ def retrieve_points(
     return nodes_with_scores
 
 
+def get_pydantic_query_engine(output_cls: BaseModel):
+    vector_store = get_qdrant_vectorstore(collection_name="gef_6_1024_96")
+    llm = TogetherLLM(model="mistralai/Mixtral-8x7B-Instruct-v0.1")
+
+    index = VectorStoreIndex.from_vector_store(vector_store)
+    query_engine = index.as_query_engine(
+        output_cls=output_cls, response_mode="tree_summarize", llm=llm
+    )
+
+    query_engine.synthesize
+
+
 def determine_private_sector_involvement(
     project_id: str, qdrant_collection: str, retry_num: int = 0
-) -> Optional[ResponseObject]:
+) -> Optional[PrivSectorClassResponseObj]:
     """Determine the level of private sector involvement for a given project ID."""
     nodes = retrieve_points(project_id, qdrant_collection)
     if not nodes:
@@ -91,7 +111,7 @@ def determine_private_sector_involvement(
 
     llm_model = TogetherLLM(model="mistralai/Mixtral-8x7B-Instruct-v0.1")
     prompt_helper = PromptHelper(context_window=32768, num_output=512)
-    summarize = TreeSummarize(verbose=True, llm=llm_model, prompt_helper=prompt_helper, output_cls=ResponseObject)  # type: ignore
+    summarize = TreeSummarize(verbose=True, llm=llm_model, prompt_helper=prompt_helper, output_cls=PrivSectorClassResponseObj)  # type: ignore
 
     try:
         response = summarize.synthesize(
@@ -99,20 +119,20 @@ def determine_private_sector_involvement(
         )
     except Exception as e:
         logger.exception(f"Error synthesizing response for project_id {project_id}")
-        if retry_num < 3:
+        if retry_num < MAX_RETRIES:
             logger.warning(f"Retrying for project_id {project_id}")
             return determine_private_sector_involvement(
                 project_id, qdrant_collection, retry_num=retry_num + 1
             )
         else:
             logger.error(
-                f"Failed to determine private sector involvement for project_id {project_id}"
+                f"Failed to determine private sector involvement for project_id {project_id} within {retry_num} retries"
             )
             return None
 
     if isinstance(response, PydanticResponse):
-        if isinstance(response.response, ResponseObject):
-            structured_response = ResponseObject(
+        if isinstance(response.response, PrivSectorClassResponseObj):
+            structured_response = PrivSectorClassResponseObj(
                 involvement_level=response.response.involvement_level,
                 secondary_involvement_level=response.response.secondary_involvement_level,
                 reason=response.response.reason,
@@ -121,6 +141,49 @@ def determine_private_sector_involvement(
 
     if structured_response:
         return structured_response
+
+    else:
+        logger.info(f"Didn't return the pydantic object but probably still worked")
+        logger.info(f"Response: {response}")
+        logger.info(f"Type: {type(response)}")
+        return None
+
+
+def generate_private_sector_summary(
+    project_id: str, qdrant_collection: str
+) -> Optional[PrivSectorSummaryResponseObj] | str:
+    """Generate a summary of the private sector involvement for a given project ID."""
+    nodes = retrieve_points(project_id, qdrant_collection, top_k=25, mmr_threshold=0.6)
+    if not nodes:
+        return None
+
+    llm_model = TogetherLLM(model="mistralai/Mixtral-8x7B-Instruct-v0.1")
+    prompt_helper = PromptHelper(context_window=32768, num_output=768)
+    summarize = TreeSummarize(verbose=True, llm=llm_model, prompt_helper=prompt_helper)  # type: ignore   , output_cls=PrivSectorSummaryResponseObj
+
+    try:
+        response = summarize.synthesize(query=QUERY_PRIVATE_SECTOR_SUMMARY, nodes=nodes)
+    except Exception as e:
+        logger.exception(f"Error synthesizing response for project_id {project_id}")
+        return None
+
+    logger.info(f"Response: {response}")
+    print(response)
+    print(type(response))
+
+    structured_response = None
+
+    if isinstance(response, PydanticResponse):
+        if isinstance(response.response, PrivSectorSummaryResponseObj):
+            structured_response = PrivSectorSummaryResponseObj(
+                summary=response.response.summary
+            )
+
+    if structured_response:
+        return structured_response
+
+    elif response:
+        return response  # type: ignore
 
     else:
         logger.info(f"Didn't return the pydantic object but probably still worked")
